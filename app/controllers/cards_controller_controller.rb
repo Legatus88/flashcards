67b76146b0cc1e57f14c4299b478b3ea6ae91ae5
Legatus88 <y1wkn8@gmail.com>
2018-06-18 16:44:25 +0400

    first results

--- app/controllers/cards_controller_controller.rb
+++ app/controllers/cards_controller_controller.rb
@@ -4,6 +4,47 @@ class CardsControllerController < ApplicationController
   end
 
   def show
-  	@card = Card.find(params[:id])
+  	@cards = Card.find(params[:id])
+  end
+
+  def new
+    @cards = Card.new
+  end
+
+  def edit
+    @cards = Card.find(params[:id])
+  end
+
+  def create
+    @cards = Card.new(card_params)
+ 
+    if @cards.save
+      redirect_to '/cards'
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @cards = Card.find(params[:id])
+ 
+    if @cards.update(card_params)
+      redirect_to '/cards'
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @cards = Card.find(params[:id])
+    @cards.destroy
+ 
+    redirect_to cards_path
+  end
+
+  private
+  
+  def card_params
+    params.require(:card).permit(:original_text, :translated_text, :review_date)
   end
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,11 @@
 class Card < ApplicationRecord
+  validates :original_text, :translated_text, :review_date, presence: true
+  validates :translated_text, presence: true, unless: :words_are_equal?
+
+  def words_are_equal?
+    ot = original_text
+    tt = translated_text.split(' ').first.capitalize
+    ot == tt
+  end
+
 end
