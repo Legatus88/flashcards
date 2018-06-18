a3311ceeec1cd82cebaff0afb396dc3e95b4f64e
Legatus88 <y1wkn8@gmail.com>
2018-06-18 22:28:12 +0400

    mission complete

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,11 +1,20 @@
 class Card < ApplicationRecord
-  validates :original_text, :translated_text, :review_date, presence: true
-  validates :translated_text, presence: true, unless: :words_are_equal?
-
-  def words_are_equal?
-    ot = original_text.split(' ').first.downcase
-    tt = translated_text.split(' ').first.downcase
-    ot == tt
+  with_options presence:true do |a|
+  	a.validates :original_text, :words_are_different?
+  	a.validates :translated_text, :words_are_different?
+  	a.validates :review_date
   end
 
+  def words_are_different?
+    ot = original_text.split(' ').first.downcase
+    tt = translated_text.split(' ').first.downcase
+    ot != tt
+  end
+
+  before_create :plus_three_days
+
+  private
+    def plus_three_days
+      self.review_date = Date.today + 3.day
+    end
 end
