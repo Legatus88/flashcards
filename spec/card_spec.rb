aed3d6a43d7313079b961a3c4408b4a6ecaef14a
Legatus88 <y1wkn8@gmail.com>
2018-06-28 16:32:15 +0400

    rspec fixes

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -2,37 +2,10 @@ require "rails_helper"
 
 RSpec.describe Card, :type => :model do 
   let(:test_card) { Card.new(original_text: 'Home', translated_text: 'Дом', review_date: Date.today) }
-
-  describe 'validations' do
-  	it 'should be not valid' do
-  	  card = Card.new(original_text: 'дом', translated_text: ' дом')
-  	  expect(card.valid?).to be false
-  	  expect(card.errors[:difference].any?).to be true
-	end
-
-	it 'should be not valid' do
-	  card = Card.new(original_text: '', translated_text: '')
-  	  expect(card.valid?).to be false
-  	  expect(card.errors[:empty_fields].any?).to be true
-	end
-
-	it 'should be not valid' do
-	  card = Card.new(original_text: 'Home', translated_text: 'Дом')
-  	  expect(card.valid?).to be false
-  	  expect(card.errors[:difference].any?).to be false
-	end
-
-	it 'should be valid' do
-	  card = Card.new(original_text: 'Home', translated_text: 'Дом', review_date: Date.today)
-  	  expect(card.valid?).to be true
-  	  expect(card.errors[:difference].any?).to be false
-  	  expect(card.errors[:empty_fields].any?).to be false
-	end
-  end
+  let(:expecting_date) { Date.today + 3.day }
 
   describe '.translation_correct?' do 
   	before { test_card }
-
 
   	it 'should be not right' do
       expect(test_card.translation_correct?('battlestart')).to be false
@@ -40,7 +13,10 @@ RSpec.describe Card, :type => :model do
 
   	it 'should be correct' do 
       expect(test_card.translation_correct?('Home')).to be true
-      expecting_date = Date.today + 3.day
+  	end
+
+  	it 'should change review_date by 3 days' do 
+      test_card.translation_correct?('Home')
       expect(test_card.review_date.to_s).to eq(expecting_date.to_s)
   	end
 
