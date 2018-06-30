8851a87b09df5d0d6252a7b63db214d6eb051976
Legatus88 <y1wkn8@gmail.com>
2018-06-30 12:39:19 +0400

    added feauture specs

---
+++ spec/factories/card.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+
+  factory :card do
+    original_text 'House'
+    translated_text 'Дом'
+    review_date Date.today
+  end
+
+end
---
+++ spec/features/check_card_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe 'checking translation' do 
+
+  let!(:card) { create(:card) }
+
+  context 'when translation is true' do
+    before { check_it('House') }
+
+    it "will tell Correct!" do
+      expect(page).to have_content 'Correct!'
+    end
+  end
+
+  context 'when translation is false' do
+    before { check_it('awda') }
+
+    it "will tell Wrong!" do
+      expect(page).to have_content 'Wrong!'
+    end
+  end
+end
