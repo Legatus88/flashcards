aa78aaf12e7f97202013488121daee4e63a710e2
Legatus88 <y1wkn8@gmail.com>
2018-07-24 22:11:16 +0400

    cards_mailer fix

--- app/mailers/cards_mailer.rb
+++ app/mailers/cards_mailer.rb
@@ -2,7 +2,7 @@ class CardsMailer < ApplicationMailer
   default from: 'notifications@flashcards.com'
 
   def pending_cards_notification
-    @users = User.includes(:cards).where("cards.review_date <= ?", Date.today).references(:cards)
+    @users = User.joins(:cards).where("cards.review_date <= now()").distinct
     @url = 'ссылка'
     @users.each do |user|
       mail(to: user.email, subject: 'Карточки ожидают ревью')
