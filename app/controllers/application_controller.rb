c683a2982ef7595f7ca51dbab2612a9fff54a940
Legatus88 <y1wkn8@gmail.com>
2018-07-31 14:58:11 +0400

    debug deleted

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :require_login, :set_locale
 
     def set_locale
-      logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
-      #I18n.locale = params[:locale] || I18n.default_locale
       I18n.locale = params[:locale] || extract_locale_from_accept_language_header
-      logger.debug "* Locale set to '#{I18n.locale}'"
     end
 
   private
