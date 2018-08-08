468f43df01bdc9b7c916043f9e57d4f3e906005d
Legatus88 <y1wkn8@gmail.com>
2018-08-08 14:50:19 +0400

    some fixes

--- Gemfile
+++ Gemfile
@@ -71,5 +71,5 @@ gem 'omniauth-twitter'
 gem "paperclip", "~> 6.0.0"
 gem "aws-sdk-s3"
 gem "damerau-levenshtein"
-gem 'whenever', require: false
 gem 'jquery-rails'
+gem 'whenever', require: false
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,25 +5,25 @@ class HomeController < ApplicationController
     @random_card = if @current_deck.nil?
       current_user.cards.expired.sample
     else
-    	current_user.cards.deck(@current_deck).sample
+      current_user.cards.deck(@current_deck).sample
     end
   end
 
   def check
     @card = Card.find(params[:id])
-  	@result = SuperMemo.new(@card).translation_correct?(params[:user_text], params[:answer_time])
+    @result = SuperMemo.new(@card).translation_correct?(params[:user_text], params[:answer_time])
 
-  	if @result
-  	  flash[:notice] = t(:correct)
+    if @result
+      flash[:notice] = t(:correct)
       flash[:correct_word] = "#{t(:correct_word)}\"#{@card.translated_text}\""
       flash[:users_word] = "#{t(:users_word)}\"#{params[:user_text]}\""
-  	else
+    else
       flash[:alert] = t(:alert)
       flash[:correct_word] = "#{t(:correct_word)}\"#{@card.translated_text}\""
-  	  flash[:users_word] = "#{t(:users_word)}\"#{params[:user_text]}\""
-  	end
+      flash[:users_word] = "#{t(:users_word)}\"#{params[:user_text]}\""
+    end
 
-  	redirect_to root_path
+    redirect_to root_path
   end
 end
 
