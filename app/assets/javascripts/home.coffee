0ae4a15f0cc5b524da2f6305b6b0eea437c9016f
Legatus88 <y1wkn8@gmail.com>
2018-08-13 15:09:34 +0400

    task 19 done

--- app/assets/javascripts/home.coffee
+++
@@ -1,4 +0,0 @@
-$ ->
-  $('.check-form').on "ajax:success", (event) ->
-     [data, status, xhr] = event.detail
-     
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,24 +9,26 @@ class HomeController < ApplicationController
   end
 
   def check
-    render layout: false
     @card = Card.find(params[:id])
     @result = SuperMemo.new(@card).translation_correct?(params[:user_text], params[:answer_time])
 
     if @result
       flash[:notice] = t(:correct)      
-      flash[:correct_word] = "#{t(:correct_word)}\"#{@card.translated_text}\""
-      flash[:users_word] = "#{t(:users_word)}\"#{params[:user_text]}\""
     else
       flash[:alert] = t(:alert)
-      flash[:correct_word] = "#{t(:correct_word)}\"#{@card.translated_text}\""
-      flash[:users_word] = "#{t(:users_word)}\"#{params[:user_text]}\""
     end
+
+    flash[:correct_word] = "#{t(:correct_word)}\"#{@card.original_text}\""
+    flash[:users_word] = "#{t(:users_word)}\"#{params[:user_text]}\""
     
-    #redirect_to root_path
-    
-    #respond_to do |format|
-    #  format.js
-    #end
+    respond_to do |format|
+      format.js
+      @current_deck = current_user.current_deck
+      @random_card = if @current_deck.nil?
+                       current_user.cards.expired.sample
+                     else
+                       current_user.cards.deck(@current_deck).sample
+                     end
+    end
   end
 end
